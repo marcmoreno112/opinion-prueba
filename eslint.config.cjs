93c01ba4ce298a0cabca13346be89f015e75fb7c
Marc Moreno <marcmoreno112@gmail.com>
2025-08-07 19:30:15 +0200

    Set workflows

---
+++ eslint.config.cjs
@@ -0,0 +1,71 @@
+const { defineConfig, globalIgnores } = require("eslint/config");
+
+const globals = require("globals");
+
+const { fixupConfigRules, fixupPluginRules } = require("@eslint/compat");
+
+const tsParser = require("@typescript-eslint/parser");
+const reactRefresh = require("eslint-plugin-react-refresh");
+const jsxA11Y = require("eslint-plugin-jsx-a11y");
+const reactHooks = require("eslint-plugin-react-hooks");
+const js = require("@eslint/js");
+
+const { FlatCompat } = require("@eslint/eslintrc");
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
+});
+
+module.exports = defineConfig([
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+      },
+
+      parser: tsParser,
+      ecmaVersion: "latest",
+      sourceType: "module",
+      parserOptions: {},
+    },
+
+    extends: fixupConfigRules(
+      compat.extends(
+        "eslint:recommended",
+        "plugin:@typescript-eslint/recommended",
+        "plugin:react/recommended",
+        "plugin:react/jsx-runtime",
+        "plugin:react-hooks/recommended",
+        "plugin:jsx-a11y/recommended",
+        "plugin:prettier/recommended",
+      ),
+    ),
+
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+
+    plugins: {
+      "react-refresh": reactRefresh,
+      "jsx-a11y": fixupPluginRules(jsxA11Y),
+      "react-hooks": fixupPluginRules(reactHooks),
+    },
+
+    rules: {
+      "react-refresh/only-export-components": "warn",
+      "no-console": "error",
+    },
+  },
+  globalIgnores([
+    "*.js",
+    "*.cjs",
+    "**/node_modules/",
+    "**/dist/",
+    "**/vite.config.*",
+    "**/jest.config.*",
+  ]),
+]);
