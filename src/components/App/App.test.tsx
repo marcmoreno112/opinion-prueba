86bf60a1068ef8345f820e1931fa5991ef0c41b8
Marc Moreno <marcmoreno112@gmail.com>
2025-08-10 17:23:22 +0200

    Create global styles

--- src/components/App/App.test.tsx
+++ src/components/App/App.test.tsx
@@ -1,17 +1,11 @@
 import { render, screen } from "@testing-library/react";
 import App from "./App";
-import { Provider } from "react-redux";
-import { store } from "../../store";
 import { describe, expect, test } from "vitest";
 
 describe("Given an App component", () => {
   describe("When rendered", () => {
     test("Then it should show a title", () => {
-      render(
-        <Provider store={store}>
-          <App />
-        </Provider>,
-      );
+      render(<App />);
 
       const titles = screen.getAllByRole("heading");
 
---
+++ src/styles/theme/theme.ts
@@ -0,0 +1,15 @@
+import { DefaultTheme } from "styled-components";
+
+const theme: DefaultTheme = {
+  colors: {
+    primary: "#2f59bc",
+    secondary: "black",
+    active: "#23AD29",
+  },
+
+  fonts: {
+    primary: "Trebuchet MS, sans-serif",
+  },
+};
+
+export default theme;
